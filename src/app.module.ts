2437aba5ec1f89806b7e7ab3c5cc50fda7092d16
LavenderEdit <gercermagden@gmail.com>
2025-12-29 16:41:43 -0500

    Add Timers module to the application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TimersModule } from './timers/timers.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { UsersModule } from './users/users.module';
 
     AuthModule,
     UsersModule,
+    TimersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
