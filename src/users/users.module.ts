b7a1b4a03c54aa299bc5fc36b46ae7a8dfdc659a
LavenderEdit <gercermagden@gmail.com>
2026-01-01 19:02:37 -0500

    Add UpdateTimerDto for updating timer data and include Setting entity in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Setting } from '../settings/entities/setting.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    controllers: [],
+    imports: [TypeOrmModule.forFeature([User, Setting])],
     providers: [UsersService],
     exports: [UsersService],
 })
