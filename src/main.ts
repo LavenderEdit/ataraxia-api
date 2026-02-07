455ab9d27856f5ef5ea4f4911c7167d754561782
LavenderEdit <gercermagden@gmail.com>
2026-02-06 23:03:36 -0500

    Update CORS configuration to support multiple frontend URLs

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(helmet());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
