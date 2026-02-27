f8eefa5e48d360517440a53690d071294c38583e
LavenderEdit <gercermagden@gmail.com>
2026-02-26 23:28:19 -0500

    chore: update version to 0.4.0 in package.json and package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Ataraxia API')
     .setDescription('API Backend para la aplicación de productividad Ataraxia')
-    .setVersion('0.3.6')
+    .setVersion('0.4.0')
     .addBearerAuth()
     .build();
 
